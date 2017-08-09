bb2fe33093d16ed1e7b9ee218fd731739fb0e488
Nutprapobsun Thongchantr <nutprapobsun.mick@gmail.com>
2017-08-09 13:52:52 +0700

    Mick update Home

---
+++ src/pages/home/home.model.ts
@@ -0,0 +1,35 @@
+export class HomeModel {
+    orders: Array<OrderItemModel>;
+    products: Array<ProductItemModel>;
+}
+
+export class OrderItemModel {
+    item: [
+        {
+            product_id: string;
+            amount: number;
+            qty: number;
+        }
+    ];
+    shop_id: string;
+    date: string;
+    net_amount: number;
+    emp_id: string;
+    receiptNo: string;
+    change: number;
+    cash: number;
+}
+export class ProductItemModel {
+    category: [
+        {
+            name: string;
+            detail: string;
+            subcate: string;
+        }
+    ];
+    image: string;
+    name: string;
+    shop_id: string;
+    price: number;
+}
+
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,17 +2,30 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { CalculatePage } from "../calculate/calculate";
 
+import { HomeModel } from './home.model';
+// import { HomeSSService } from './home.service';
+import 'rxjs/Rx';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  homemo: HomeModel = new HomeModel();
+  constructor(public navCtrl: NavController,
+    //  public homeservice: HomeService
+  ) {
 
   }
   gotocalculate() {
     this.navCtrl.push(CalculatePage)
+  }
+  ionViewDidLoad() {
+    // this.homeservice
+    //   .getData()
+    //   .then(data => {
+    //     this.homemo.products = data.products;
+    //     this.homemo.orders = data.orders;
+    //   });
   }
 
 }
